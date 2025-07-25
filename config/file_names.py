cceb73b78a1533b0c97f6bfb735e97916ce3ac51
Coder Joan <codejoan@op.pl>
2025-07-25 23:23:32 +0200

    Add /config

---
+++ config/file_names.py
@@ -0,0 +1,6 @@
+MAP_FILE_NAMES = {
+    "Kraje podróży Abrahama": "kraje_podrozy_abrahama.jpg",
+    "Mapa plemion izraelskich": "mapa_plemion_izraelskich.jpg",
+    "Palestyna w czasach Nowego Testamentu": "palestyna_w_czasach_nowego_testamentu.jpg",
+    "Podróże Apostoła Pawła": "podroze_apostola_pawla.jpg"
+}
---
+++ config/paths.py
@@ -0,0 +1,4 @@
+MAPS = "resources/maps/"
+TRANSLATIONS = "resources/bibles/translations.json"
+POLISH_BOOK_NAMES = "resources/books/polish_book_names.json"
+ALTERNATIVE_BOOK_NAMES = "resources/books/alternative_book_names.json"
