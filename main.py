ee99ebed8f4ac74fbce0669ce0b5d8114b0d7c3e
Coder Joan <codejoan@op.pl>
2025-08-15 16:46:45 +0200

    Added new commands

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from dotenv import load_dotenv
 from discord.ext import commands
 from events.on_ready import setup_ready_event
 from events.on_message import setup_message_event
+from events.on_guild_join import setup_guild_join_event
 from events.on_command_error import setup_command_error_event
 
+from commands.cleardailyverse import cleardailyverse
+from commands.cleartranslation import cleartranslation
 from commands.compare import compare
 from commands.dailyverse import dailyverse
 from commands.help import help
 from commands.information import information
 from commands.invite import invite
 from commands.maps import maps
+from commands.mysettings import mysettings
 from commands.passage import passage
 from commands.random import random
-from commands.removeuserdata import removeuserdata
 from commands.search import search
+from commands.setdailyverse import setdailyverse
 from commands.setversion import setversion
 from commands.stats import stats
 from commands.support import support
@@ -25,16 +29,19 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix='!', intents=intents)
 
+client.tree.add_command(cleardailyverse)
+client.tree.add_command(cleartranslation)
 client.tree.add_command(compare)
 client.tree.add_command(dailyverse)
 client.tree.add_command(help)
 client.tree.add_command(information)
 client.tree.add_command(invite)
 client.tree.add_command(maps)
+client.tree.add_command(mysettings)
 client.tree.add_command(passage)
 client.tree.add_command(random)
-client.tree.add_command(removeuserdata)
 client.tree.add_command(search)
+client.tree.add_command(setdailyverse)
 client.tree.add_command(setversion)
 client.tree.add_command(support)
 client.tree.add_command(versions)
@@ -43,6 +50,7 @@ client.add_command(stats)
 load_dotenv()
 setup_ready_event(client)
 setup_message_event(client)
+setup_guild_join_event(client)
 setup_command_error_event(client)
 
 client.run(os.environ['TOKEN'])
