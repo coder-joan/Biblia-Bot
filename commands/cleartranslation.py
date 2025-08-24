a1ffe4ee5bc76b02246f5d51e149d44b5c2c5e17
Coder Joan <codejoan@op.pl>
2025-08-24 18:44:00 +0200

    Add cleartranslation.py

---
+++ commands/cleartranslation.py
@@ -0,0 +1,29 @@
+import discord
+
+from discord import app_commands
+from config.colors import SUCCESS_COLOR, ERROR_COLOR
+from services.user_translation_db import get_user_settings, remove_user_translation
+
+@app_commands.command(name="cleartranslation", description="Usuwa domyślny przekład Pisma Świętego")
+async def cleartranslation(interaction: discord.Interaction):
+    await interaction.response.defer(ephemeral=True)
+
+    user_id = interaction.user.id
+    user_data = get_user_settings(user_id)
+
+    if user_data:
+        remove_user_translation(user_id)
+
+        embed = discord.Embed(
+            title="Usunięto pomyślnie",
+            description=("Twój domyślny przekład Pisma Świętego został usunięty"),
+            color=SUCCESS_COLOR
+        )
+        await interaction.followup.send(embed=embed, ephemeral=True)
+    else:
+        error_embed = discord.Embed(
+            title="Brak danych",
+            description="Nie znaleziono twoich danych w bazie danych",
+            color=ERROR_COLOR
+        )
+        await interaction.followup.send(embed=error_embed, ephemeral=True)
