4fd79628c1bbb37a748b510a54843175b2ed42d3
Coder Joan <codejoan@op.pl>
2025-08-31 14:08:47 +0200

    Added new stats

--- commands/stats.py
+++ commands/stats.py
@@ -2,14 +2,16 @@ import discord
 
 from discord.ext import commands
 from config.colors import STANDARD_COLOR
-from services.user_settings_db import get_user_count
+from services.user_translation_db import get_user_count
+from services.dailyverse_settings_db import get_dailyverse_user_count
 
 @commands.command(name="stats")
 async def stats(ctx):
     embed = discord.Embed(
         title="Statystyki",
         description=f"Liczba serwerów: **{len(ctx.bot.guilds)}**\n"
-                    f"Liczba użytkowników: **{get_user_count()}**",
+                    f"Liczba użytkowników: **{get_user_count()}**\n"
+                    f"Użytkownicy automatyzacji wersetu dnia: **{get_dailyverse_user_count()}**",
         color=STANDARD_COLOR
     )
     await ctx.send(embed=embed)
